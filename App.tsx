76215a1fca5d33888d04b6a3168602909ed6b614
yaner <e0970478@u.nus.edu>
2025-11-25 09:43:13 +0800

    config fix

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { TamaguiProvider, View } from '@tamagui/core'
+import type { TamaguiInternalConfig } from '@tamagui/web'
 import { config } from './tamagui.config' // your configuration
 
 export default function App() {
   return (
-    <TamaguiProvider config={config}>
+    <TamaguiProvider config={config as unknown as TamaguiInternalConfig}>
       <View width={200} height={200} backgroundColor="$background" />
     </TamaguiProvider>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import { TamaguiProvider, TamaguiProviderProps } from 'tamagui'
 export * from './components/Button'
 export * from './components/StepIndicator'
 export * from './components/FormGroup'
-export { config as tamaguiConfig } from '../tamagui.config'
+import { config as tamaguiConfig } from '../tamagui.config'
 
 export function DesignSystemProvider(props: Omit<TamaguiProviderProps, 'config'> & { children?: React.ReactNode }) {
-  // consumers can use this to ensure the provider uses your config
-  // eslint-disable-next-line react/jsx-no-constructed-context-values
-  return <TamaguiProvider {...(props as TamaguiProviderProps)} config={require('../tamagui.config').config}>{props.children}</TamaguiProvider>
+  const providerProps = props as TamaguiProviderProps
+  return (
+    <TamaguiProvider {...providerProps} config={tamaguiConfig}>
+      {props.children}
+    </TamaguiProvider>
+  )
 }
